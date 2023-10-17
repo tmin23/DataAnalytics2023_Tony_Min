0a33793ff74c538579e061e9105f5578b468c560
Tony Min <mint@rpi.edu>
2023-10-17 14:30:07 -0400

    Assignment 3
    Not finished just setting up github then will submit lab into repo

--- Labs/lab1/Lab1_part2.R
+++ Labs/lab1/Lab1_part2.R
@@ -20,5 +20,59 @@ attributes(mm)
 mm$coefficients
 
 # ggplot examples
+library(ggplot2)
+head(mtcars)
+plot(mtcars$wt, mtcars$mpg)
+qplot(mtcars$wt, mtcars$mpg)
+qplot(wt, mpg, data = mtcars) # Same Plot
+ggplot(mtcars, aes(x = wt, y = mpg)) + geom_point()
+plot(pressure$temperature, pressure$pressure, type = "l")
+points(pressure$temperature, pressure$pressure)
+
+lines(pressure$temperature, pressure$pressure/2, col = "red")
+points(pressure$temperature, pressure$pressure/2, col = "blue")
+
+qplot(pressure$temperature, pressure$pressure, geom="line")
+qplot(temperature, pressure, data = pressure, geom = "line") # Same plot
+
+ggplot(pressure, aes(x=temperature, y = pressure)) + geom_line() + geom_point()
+ggplot(pressure, aes(x=temperature, y = pressure)) + geom_line() + geom_point()
+ 
+# Create Bar Graphs
+barplot(BOD$demand, names.arg = BOD$Time)
+table(mtcars$cyl)
+barplot(table(mtcars$cyl)) # generate bar plot of counts
+qplot(mtcars$cyl)
+qplot(factor(mtcars$cyl))
+# Bar graph of counts
+qplot(factor(cyl), data = mtcars)
+ggplot(mtcars, aes(x=factor(cyl))) + geom_bar()
+
+# Creating Histogram
+hist(mtcars$mpg)
+hist(mtcars$mpg, breaks = 10) # approximate number of bins with breaks
+hist(mtcars$mpg, breaks = 5)
+hist(mtcars$mpg, breaks = 12)
+qplot(mpg, data = mtcars, binwidth = 4)
+ggplot(mtcars, aes(x=mpg)) + geom_histogram(binwidth = 4)
+ggplot(mtcars, aes(x=mpg)) + geom_histogram(binwidth = 5)
+                                            
+# Creating Box plot using ggplot
+plot(ToothGrowth$supp, ToothGrowth$len)
+
+boxplot(len ~ supp + dose, data = ToothGrowth)
+qplot(ToothGrowth$supp, ToothGrowth$len, geom = "boxplot")
+
+qplot(supp, len, data = ToothGrowth, geom = "boxplot")
+
+ggplot(ToothGrowth, aes(x = supp, y = len)) + geom_boxplot()
+
+qplot(interaction(ToothGrowth$supp, ToothGrowth$dose), ToothGrowth$len, geom = "boxplot")
+qplot(interaction(supp, dose), len, data = ToothGrowth, geom = "boxplot")
+
+ggplot(ToothGrowth, aes(x = interaction(supp, dose), y = len)) + geom_boxplot()
+
+
+
 
 
