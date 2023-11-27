ac22caf35c31c303f52807f9c5df558ddfb0ba98
Tony Min <mint@rpi.edu>
2023-11-27 18:48:03 -0500

    Dataset 2
    WIP

--- Assignments/Assignment 7/Dataset2/dataset2.R
+++ Assignments/Assignment 7/Dataset2/dataset2.R
@@ -2,5 +2,20 @@ library(rpart)
 library(rpart.plot)
 library(randomForest)
 
-edu <- read.csv("DATA.csv", header = TRUE)
-View(edu)
+cancer <- read.csv("sobar-72.csv", header = TRUE)
+View(cancer)
+
+# EDA
+summary(cancer)
+
+
+summary(cancer$behavior_eating)
+boxplot(cancer$behavior_eating, main = "Behavior Eating Boxplot")
+hist(cancer$behavior_eating, main = "Behavior Eating Boxplot")
+
+summary(cancer$motivation_strength)
+boxplot(cancer$motivation_strength, main = "Motivation_Strength Boxplot")
+hist(cancer$motivation_strength, main = "Motivation_Strength Histogram")
+
+
+summary(cancer$socialSupport_instrumental)
